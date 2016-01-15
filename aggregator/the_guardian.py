7a1e6a36e0e5ebd9d91901aed5a086530f0d6576
ueg1990 <uehtesham90@gmail.com>
2016-01-15 18:46:21 -0500

    Re-factor The Guardian aggregator w.r.t. new contract

--- aggregator/the_guardian.py
+++ aggregator/the_guardian.py
@@ -13,35 +13,33 @@ class TheGuardian(Aggregator):
     source = 'The Guardian'
 
     def extract(self):
-        url = self.generate_url()
+        url = self._generate_url()
         soup = make_soup(url)
-        divs =  iter(soup.find_all('div', {'class': 'fc-item__content'}))
-        valid_hrefs = (href for href in self._extract_valid_href(divs))
-        articles = (self.crawl(href) for href in valid_hrefs)
-        return list(articles)
+        divs =  iter(soup.find_all('div', {'class': 'fc-item__container'}))
+        articles = (self.crawl(div) for div in divs)
+        return list(article for article in articles if article is not None)
 
-    def _extract_valid_href(self, divs):
-        for div in divs:
-            href = div.find('a')['href']
-            if self._is_valid_article(href):
-                yield href
-
-    def crawl(self, url):
+    def crawl(self, tag):
         try:
-            soup = make_soup(url)
-            title = self.get_title(soup)
-            div = soup.find('div', {'class': 'content__meta-container'})
+            anchor = tag.find('a')
+            url = self.get_url(anchor)
+            if not url:
+                return None
+            title = self.get_title(anchor)
+            div = make_soup(url)
+            div = div.find('div', {'class': 'content__meta-container'})
             author = self.get_author(div)
             date_published = self.get_date_published(div)
             return Article(TheGuardian.source, title, url, author, date_published)
         except exceptions.WebCrawlException as e:
-            return InvalidArticle(TheGuardian.source, url, e)
+            return InvalidArticle(TheGuardian.source, e)
 
-    def get_author(self, soup):
-        author = soup.find('a', {'rel': 'author'})
-        if author and author.text:
+    def get_author(self, tag):
+        try:
+            author = tag.find('a', {'rel': 'author'})
             return author.text.strip()
-        raise exceptions.AuthorNotFoundException
+        except AttributeError as e:
+            raise exceptions.AuthorNotFoundException
 
     def get_date_published(self, soup):
         try:
@@ -55,13 +53,21 @@ class TheGuardian(Aggregator):
         except (IndexError, AttributeError, ValueError):
             raise exceptions.DatePublishedNotFoundException
 
-    def get_title(self, soup):
-        title = soup.find('h1', {'class': 'content__headline'})
-        if title and title.text:
-            return title.text.strip()
-        raise exceptions.TitleNotFoundException
+    def get_title(self, tag):
+        try:
+            return tag.text.strip()
+        except AttributeError as e:
+            raise exceptions.TitleNotFoundException
 
-    def generate_url(self):
+    def get_url(self, tag):
+        try:
+            url = tag['href']
+            return url if self._is_valid_article(url) else None
+        except (KeyError, TypeError):
+            raise exceptions.UrlNotFoundException
+
+
+    def _generate_url(self):
         current_date = self._current_date()
         url = '{0}/{1}/{2}/{3}/all'.format(TheGuardian.base_url, *current_date)
         return url
