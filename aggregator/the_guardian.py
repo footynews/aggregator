6725ff1686e1ddbf2342f7442dd659f2959e4987
ueg1990 <uehtesham90@gmail.com>
2016-01-16 00:31:38 -0500

    Modify date retrievel

--- aggregator/the_guardian.py
+++ aggregator/the_guardian.py
@@ -2,8 +2,8 @@ import datetime
 
 from urllib.parse import urlparse
 
-from aggregator.base import Aggregator, Article, InvalidArticle, make_soup
 from aggregator import exceptions
+from aggregator.base import Aggregator, Article, InvalidArticle, make_soup
 from aggregator.utils.calendar import month_to_code, code_to_month
 
 
@@ -16,56 +16,47 @@ class TheGuardian(Aggregator):
         url = self._generate_url()
         soup = make_soup(url)
         divs =  iter(soup.find_all('div', {'class': 'fc-item__container'}))
-        articles = (self.crawl(div) for div in divs)
-        return list(article for article in articles if article is not None)
+        self.articles = (self.crawl(div) for div in divs)
+        return super().extract()
 
     def crawl(self, tag):
         try:
             anchor = tag.find('a')
             url = self.get_url(anchor)
+            # Added here because URLs are filtered via method _is_valid_article
             if not url:
                 return None
             title = self.get_title(anchor)
-            div = make_soup(url)
-            div = div.find('div', {'class': 'content__meta-container'})
-            author = self.get_author(div)
-            date_published = self.get_date_published(div)
-            return Article(TheGuardian.source, title, url, author, date_published)
+            if title:
+                div = make_soup(url)
+                div = div.find('div', {'class': 'content__meta-container'})
+                author = self.get_author(div.find('a', {'rel': 'author'}))
+                date_published = self.get_date_published(None)
+                return Article(TheGuardian.source, title, url, author,
+                               date_published)
         except exceptions.WebCrawlException as e:
             return InvalidArticle(TheGuardian.source, e)
 
-    def get_author(self, tag):
-        try:
-            author = tag.find('a', {'rel': 'author'})
-            return author.text.strip()
-        except AttributeError as e:
-            raise exceptions.AuthorNotFoundException
-
-    def get_date_published(self, soup):
-        try:
-            date_published = soup.find('p', {'class': 'content__dateline'})
-            date_published = date_published.findChildren()[0].text
-            _, *date_published, _, _ = date_published.split()
-            date_published[1] = code_to_month[date_published[1][:3].lower()]
-            date_published.reverse()
-            date_published = datetime.datetime(*map(int, date_published)).date()
-            return date_published
-        except (IndexError, AttributeError, ValueError):
-            raise exceptions.DatePublishedNotFoundException
-
-    def get_title(self, tag):
-        try:
-            return tag.text.strip()
-        except AttributeError as e:
-            raise exceptions.TitleNotFoundException
+    def get_date_published(self, tag):
+        # Given structure of HTML pages is w.r.t. date and since we only web
+        # scrape one specific page per day, we can just return current date
+        return datetime.date.today()
+        # try:
+        #     date_published = tag.findChildren()[0].text
+        #     _, *date_published, _, _ = date_published.split()
+        #     date_published[1] = code_to_month[date_published[1][:3].lower()]
+        #     date_published.reverse()
+        #     date_published = datetime.datetime(*map(int, date_published)).date()
+        #     return date_published
+        # except (IndexError, AttributeError, ValueError):
+        #     raise exceptions.DatePublishedNotFoundException
 
     def get_url(self, tag):
         try:
             url = tag['href']
-            return url if self._is_valid_article(url) else None
+            return url #if self._is_valid_article(url) else None
         except (KeyError, TypeError):
             raise exceptions.UrlNotFoundException
-
 
     def _generate_url(self):
         current_date = self._current_date()
