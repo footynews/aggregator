75dea792cc5926da57b21072e2ca6d236077c149
ueg1990 <uehtesham90@gmail.com>
2016-01-28 22:36:55 -0500

    Install sqlalchemy and define model for articles

---
+++ footynews/db/models.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class Article(Base):
+    """Model for Articles"""
+    __tablename__ = 'articles'
+
+    _id = Column(Integer, primary_key=True)
+    source = Column(String)
+    title = Column(String)
+    url = Column(String)
+    author = Column(String)
+    date_published = Column(DateTime)
+
+    def __init__(self, article):
+        self.source = article.source
+        self.title = article.title
+        self.url = article.url
+        self.author = article.author
+        self.date_published = article.date_published
+
+    def __repr__(self):
+        return "<Article(source={0})>"
+
+engine = create_engine('')
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine)
+db_session = DBSession()
