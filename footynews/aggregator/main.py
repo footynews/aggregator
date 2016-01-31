ecaaa5b6230a2c0bd3f100918a054e4ced12301f
ueg1990 <uehtesham90@gmail.com>
2016-01-31 18:47:37 -0500

    Define class to generate stats and report daily

--- footynews/aggregator/main.py
+++ footynews/aggregator/main.py
@@ -8,6 +8,7 @@ from pluginbase import PluginBase
 from footynews.aggregator.base import Article, InvalidArticle
 from footynews.aggregator.tasks.send_email import send_email
 from footynews.aggregator.utils import generate_stats
+from footynews.daily_report import DailyReport
 from footynews.db.models import db_session
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -16,21 +17,23 @@ plugin_base = PluginBase(package='web_scraping')
 plugin_source = plugin_base.make_plugin_source(searchpath=[os.path.join(here,
                                                'web_scraping_plugins')])
 
-from_addr = os.environ.get('FOOTYNEWS_ADMIN_EMAIL')
-password = os.environ.get('FOOTYNEWS_ADMIN_PASSWORD')
-subject = 'FootyNews Web Scraping Report {0}'
-
+daily_report = DailyReport(datetime.date.today())
 
 def main():
-    current_date = datetime.date.today()
     articles = []
     for plugin in plugin_source.list_plugins():
             source = plugin_source.load_plugin(plugin).setup()
-            articles.extend(list(getattr(source, 'extract')()))
-    stats = generate_stats(articles)
-    report = generate_report((article for article in articles 
-                              if isinstance(article, InvalidArticle)),
-                              current_date)
+            #articles.extend(list(getattr(source, 'extract')()))
+            for article in getattr(source, 'extract')():
+                if isinstance(article, Article):
+                    # store in database
+                    pass
+                elif isinstance(article, InvalidArticle):
+                    daily_report.update_report(article)
+                daily_report.update_stats(article)
+    if datetime.datetime.now.hour == 23:
+        daily_report.email_report()
+        daily_report.reset()
 
 
 if __name__ == '__main__':
---
+++ footynews/daily_report.py
@@ -0,0 +1,57 @@
+import os
+from collections import defaultdict
+
+from footynews.aggregator.base import Article, InvalidArticle
+from footynews.send_email import send_email
+
+body_template_text = ''
+
+body_template_html = ''
+
+class DailyReport:
+
+    def __init__(self, current_date):
+        self.current_date = current_date
+        self.stats = defaultdict(int)
+        self.invalid_articles = []
+
+    def update_stats(self, article):
+        self.stats['total'] += 1
+        if isinstance(article, Article):
+            self.stats['valid_articles'] += 1
+            sef.stats['valid_{0}'.format(
+                article.source.lower().replace(' ', '_'))] += 1
+        elif isinstance(article, InvalidArticle):
+            self.stats['invalid_articles'] += 1
+            sef.stats['invalid_{0}'.format(
+                article.source.lower().replace(' ', '_'))] += 1
+
+    def update_report(self, article):
+        self.invalid_articles.append(article)
+
+    def generate_report(self):
+        report_name = ('footynews_invalid_articles_daily_report_{0}'
+                       .format(self.current_date))
+        report_path = os.path.join('/tmp', report_name)
+        with open(report_path, 'w') as f:
+            writer = csv.writer(f)
+            write.writerow(['Source', 'Exception', 'Message', 'Tag'])
+            writer.writerows(self.invalid_articles)
+        return report_path
+
+    def reset():
+        self.stats = {}
+        self.invalid_articles = []
+
+    def email_report(self):
+        from_addr = os.environ.get('FOOTYNEWS_ADMIN_EMAIL')
+        password = os.environ.get('FOOTYNEWS_ADMIN_PASSWORD')
+        to_addr = os.environ.get('FOOTYNEWS_ADMIN_EMAIL')
+        subject = 'FootyNews Web Scraping Report {0}'.format(self.current_date)
+        report_path = self.generate_report()
+        send_email(from_addr, password, to_addr, subject, body_template_text,
+                   body_template_html, self.stats, report_name)
+        self.delete_report(report_path)
+
+    def delete_report(self, report_path):
+        os.remove(report_path)
